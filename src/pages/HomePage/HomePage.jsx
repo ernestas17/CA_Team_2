e03e8ac1cd9cf8c054297d1ea86d4231881532f5
DovileSteniukynaite <dovidovile@gmail.com>
2022-08-21 15:07:13 +0300

    Created HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import HeaderSection from './components/HeaderSection';
+import QuoteSection from './components/QuoteSection';
+import ProcessSection from './components/ProcessSection';
+import OfferSection from './components/OfferSection';
+import BrandsSection from './components/BrandsSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import FAQSection from './components/FAQSection';
+import CallToAction from '../../components/organisms/CallToAction';
+import Experience from '../../components/atoms/Experience';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main>
+      <HeaderSection />
+      <QuoteSection />
+      <ProcessSection />
+      <OfferSection />
+      <CallToAction />
+      <Experience />
+      <BrandsSection />
+      <TestimonialsSection />
+      <FAQSection />
+    </main>
+  );
 };
 
 export default HomePage;
